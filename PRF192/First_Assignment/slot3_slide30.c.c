685af8d30d5106f5211de198b40241c1f92c1405
anhshidou <ranin.fox.87@gmail.com>
2024-01-08 09:44:35 +0700

    Add files via upload

---
+++ PRF192/First_Assignment/slot3_slide30.c.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(){
+	printf ("\a");
+	printf ("He said that \"I love you\" \n"); //Li do tai sao phai them dau nhay la vi: 1 dong printf khong the co 2 dau nhay, vi the nen ta can phai bien cau I love you thanh 1 dong rieng biet bang dau \ 
+	printf ("She says 'A' \n");
+	printf ("My file: C:\\t1\\t111\\new_year.txt");
+}
---
+++ PRF192/First_Assignment/slot3_slide33.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(){
+	double PI = 3.14;
+	printf ("%lf\n", PI*3*3);
+}
